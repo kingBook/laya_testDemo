73f6ed7883b67acf878eef7821f97c2a19a0bc53
pq <pq99880022@gmail.com>
2025-08-14 19:37:20 +0800

    物理

--- assets/scenes/testPhysics2d/Box.ts
+++ assets/scenes/testPhysics2d/Box.ts
@@ -5,14 +5,20 @@ const { regClass, property } = Laya;
 export class Box extends Laya.Script {
 
     onTriggerEnter(other: Laya.PhysicsColliderComponent | Laya.ColliderBase, self?: Laya.ColliderBase, contact?: any): void {
-        console.log("onTriggerEnter:", other, self, contact);
+        console.log("onTriggerEnter:--------", other, self, contact);
+
+        const physicsFactory: any = Laya.Physics2D.I._factory;
+        const worldManifold = new physicsFactory._box2d.b2WorldManifold();
+        contact.GetWorldManifold(worldManifold);
+        console.log("worldManifold:", worldManifold);
+
     }
 
     onTriggerExit(other: Laya.PhysicsColliderComponent | Laya.ColliderBase, self?: Laya.ColliderBase, contact?: any): void {
-        console.log("onTriggerExit:", other, self, contact);
+        //console.log("onTriggerExit:-----", other, self, contact);
     }
 
     onTriggerStay(other: Laya.PhysicsColliderComponent | Laya.ColliderBase, self?: Laya.ColliderBase, contact?: any): void {
-        console.log("onTriggerStay:", other, self, contact);
+        //console.log("onTriggerStay:", other, self, contact);
     }
 }
--- assets/scenes/testPhysics2d/TestBody.ts
+++ assets/scenes/testPhysics2d/TestBody.ts
@@ -1,47 +1,54 @@
+import { Separator } from "./Separator";
 
 const { regClass, property } = Laya;
 
 @regClass()
 export class TestBody extends Laya.Script {
 
+
     @property({ type: Laya.Sprite })
     polygon_graphics: Laya.Sprite;
 
-    private _bodyb2: any;
 
     onAwake(): void {
+        const physicsFactory: any = Laya.Physics2D.I._factory;
+        console.log(physicsFactory);
+        
+        this.convert();
+    }
 
-        const xys = [
-            -31, 37,
-            0, 0,
-            62, -30,
-            55, 54,
-            147, 71,
-            243, 42,
-            346, 30,
-            324, 102,
-            308, 231,
-            231, 188,
-            237, 132,
-            117, 131,
-            0, 100
-        ];
+    /** 转换 graphics 多边形顶点到  Laya.PolygonShape2D 数组 */
+    private convert(): void {
+        const drawPolyCmd = this.polygon_graphics.graphics.cmds[0] as Laya.DrawPolyCmd;
 
-        const body = this.polygon_graphics.addComponent(Laya.RigidBody);
+        // 将[x,y,x,y,...]转为[{x,y}, {x,y},..]
+        const points: { x: number, y: number }[] = [];
+        for (let i = 0; i < drawPolyCmd.points.length; i += 2) {
+            points.push({ x: drawPolyCmd.points[i], y: drawPolyCmd.points[i + 1] });
+        }
 
-        //body.shapes = [poly];
-        const bodyb2 = body.getBox2DBody();
-        this._bodyb2 = bodyb2;
+        // 凸分解
+        const poly2ds = Separator.separate(points);
 
-        //const polyb2 = new b2PolygonShape();Laya.Box2DShape
-        // polyb2.SetAsBox(40 / 50, 40 / 50);
-        //polyb2.Set(xys);
+        // 将 [{x,y}, {x,y},..] 转为 [x,y,x,y,...]，并创建 PolygonShape2D 数组
+        const shapes: Laya.PolygonShape2D[] = [];
+        poly2ds.forEach(pts => {
+            const pol = new Laya.PolygonShape2D();
+            const datas: number[] = [];
+            pts.forEach(pt => {
+                datas.push(pt.x, pt.y);
+            });
+            pol.datas = datas;
+            shapes.push(pol);
+        });
+        this.polygon_graphics.getComponent(Laya.RigidBody).shapes = shapes;
 
-        // bodyb2.CreateFixture(polyb2, 1);
+        //清除 graphcis
+        this.polygon_graphics.graphics.clear();
     }
 
     onUpdate(): void {
-        //console.log("pos", this._bodyb2.GetPosition());
+
     }
 
 }
