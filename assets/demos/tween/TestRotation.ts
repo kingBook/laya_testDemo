0cc2e67ba56fb0245c9b47eec2e3f58b2f3d95aa
pq <pq99880022@gmail.com>
2025-09-23 17:24:53 +0800

    缓动旋转测试

--- assets/demos/tween/TestRotation.ts
+++ assets/demos/tween/TestRotation.ts
@@ -3,11 +3,13 @@ const { regClass, property } = Laya;
 @regClass()
 export class TestRotation extends Laya.Script {
 
-    @property({type:Laya.Sprite})
-    target:Laya.Sprite;
+    @property({ type: Laya.Sprite })
+    target: Laya.Sprite;
 
     private _tween: Laya.Tween;
-
+    private _isTweening: boolean;
+    private _isRotating: boolean;
+    private _rotationSpeed: number;
 
     private onComplete(msg: string): void {
         console.log("onComplete: msg:", msg);
@@ -15,8 +17,25 @@ export class TestRotation extends Laya.Script {
 
     onKeyDown(evt: Laya.Event): void {
         if (evt.key === "h") {
-            console.log("开始缓动");
-            this._tween=Laya.Tween.create(this.target).duration(5000).go("rotation",-10,720).ease(Laya.Ease.sineOut);
+            console.log("开始旋转");
+            this._rotationSpeed = 5//(Math.random() > 0.5 ? 1 : -1) * 5;
+            this._isRotating = true;
+        } else if (evt.key === 'j') {
+            const targetRotation = Math.random()*360;
+            console.log("开始缓动", "rotation",this.target.rotation, "targetRotation", targetRotation);
+            this._tween = Laya.Tween.create(this.target).duration(5000).to("rotation", targetRotation).ease(Laya.Ease.sineOut);
+            this._isTweening=true;
+        }
+    }
+
+    onUpdate(): void {
+        if (this._isTweening) {
+
+            return;
+        }
+
+        if (this._isRotating) {
+            this.target.rotation+=this._rotationSpeed;
         }
     }
 }
