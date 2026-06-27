89d006e2c12982b9708b847738be86826274d477
pq <pq99880022@gmail.com>
2026-06-27 18:16:31 +0800

    test mouse joint

---
+++ assets/demos/物理2d/鼠标关节/TestMouseJoint.ts
@@ -0,0 +1,16 @@
+const { regClass, property } = Laya;
+
+@regClass()
+export class TestMouseJoint extends Laya.Script {
+
+    @property({ type: Laya.MouseJoint, private: false })
+    private _mouseJoint: Laya.MouseJoint;
+
+    onAwake(): void {
+
+    }
+
+    onUpdate(): void {
+        
+    }
+}
