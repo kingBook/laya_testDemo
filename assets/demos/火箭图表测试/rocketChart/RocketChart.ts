e143b8cd81312bc1b0f48a1da6efeee2f96a23e7
pq <pq99880022@gmail.com>
2026-08-05 16:50:52 +0800

    RocketChart——修正立即更新状态到指定的时间、倍数时的纠正算法

--- assets/demos/火箭图表测试/rocketChart/RocketChart.ts
+++ assets/demos/火箭图表测试/rocketChart/RocketChart.ts
@@ -234,16 +234,22 @@ export class RocketChart extends Laya.Script {
     public updateStatusToTime(time: number, multiplier?: number): void {
         // console.time("draw");
 
+        if (!this._shapeBox.visible) this._shapeBox.visible = true; // 图形盒
+        if (!this._multiplierBox.visible) this._multiplierBox.visible = true; // 倍数盒
+
         // 修正时间、倍数保证对应(倍数优先)
         if (!isNaN(multiplier)) {
-            const tempMultiplier: string = RocketChart.timeToMultiplier(time, this._initSpeed, this._acceleration).toFixed(2);
-            if (tempMultiplier != multiplier.toFixed(2)) {
+            const fixedPoint = (multiplier % 1 === 0) ? 0 : 2; // 纠正时判断的小数位（是整数则0， 否则2）
+            const tempMultiplier: string = RocketChart.timeToMultiplier(time, this._initSpeed, this._acceleration).toFixed(fixedPoint);
+            if (tempMultiplier != multiplier.toFixed(fixedPoint)) {
+                // console.log("testTime 纠正时间",
+                //     "原时间", time,
+                //     "纠正后时间", RocketChart.multiplierToTime(multiplier, this._initSpeed, this._acceleration),
+                //     "原倍数", multiplier.toFixed(fixedPoint),
+                //     "纠正后倍数", tempMultiplier);
                 time = RocketChart.multiplierToTime(multiplier, this._initSpeed, this._acceleration);
             }
         }
-
-        if (!this._shapeBox.visible) this._shapeBox.visible = true; // 图形盒
-        if (!this._multiplierBox.visible) this._multiplierBox.visible = true; // 倍数盒
 
         // 时间
         this._time = time;
