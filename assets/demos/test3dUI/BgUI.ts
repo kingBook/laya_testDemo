4d8b0c60132f959477b273ef9b93dbfc2d396c0c
pq <pq99880022@gmail.com>
2026-08-14 18:40:27 +0800

    3dui test 3

--- assets/demos/test3dUI/BgUI.ts
+++ assets/demos/test3dUI/BgUI.ts
@@ -1,5 +1,5 @@
 const { regClass, property } = Laya;
-
+const Vector3 = Laya.Vector3;
 
 @regClass()
 export class BgUI extends Laya.Script {
@@ -23,14 +23,45 @@ export class BgUI extends Laya.Script {
     }
 
     private onStageResize(e: Laya.Event): void {
-        const tan = Math.tan(this._camera.fieldOfView / 2 * Math.PI / 180);
-        //this._camera.aspectRatio
-        //console.log(this._camera.clientWidth, this._camera.clientHeight);
+        // 设置背景平面的距离 --------------------------------------------------------
+        this.owner.transform.position = new Vector3(0, 0, -10);
 
-        this.owner.transform.position = new Laya.Vector3(0, 0, -10);
-        const scaleX = (this._ui3d.resolutionRate * this._ui3d.scale.x) / 100;
-        const scaleY = (this._ui3d.resolutionRate * this._ui3d.scale.y) / 100;
+
+        // 计算缩放 --------------------------------------------------------
+        // - 计算背景图所在锥体截面的宽
+        const distance = this._camera.transform.position.z - this._ui3d.owner.transform.position.x;
+        const tan = Math.tan(this._camera.fieldOfView / 2 * Math.PI / 180);
+        const height = tan * distance * 2;
+        const width = this._camera.aspectRatio * height;
+
+        const bgWidth = this._ui3d.resolutionRate * this._ui3d.scale.x;
+        const bgHeight = this._ui3d.resolutionRate * this._ui3d.scale.y;
+
+        let scaleX = bgWidth / 70;
+        let scaleY = bgHeight / 70;
+
+        const sx = (width * 100) / bgWidth;
+        const sy = (height * 100) / bgHeight;
+
+        // - 匹配宽高
+        // scaleX *= sx;
+        // scaleY *= sy;
+
+        // - 匹配宽
+        scaleX *= sx;
+        scaleY *= sx;
+
+        // - 匹配高
+        // scaleX *= sy;
+        // scaleY *= sy;
+
         this.owner.transform.localScale = new Laya.Vector3(scaleX, scaleY, 1);
+
+
+        // 对齐方式  --------------------------------------------------------
+        // - 居中对齐（默认）
+
+        // - 顶对齐
 
 
 
